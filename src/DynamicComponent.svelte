f3ce0eafe8d99bb92a65c7300b5dc1b4e79a421c
jimafisk <jimafisk@gmail.com>
2020-09-29 16:09:28 -0400

    Add fallback HTML and hydrate it.

---
+++ src/DynamicComponent.svelte
@@ -0,0 +1,14 @@
+<section>
+  <h3>This is a Dynamic Component</h3>
+  <ul>
+    <li>Item 1</li>
+    <li>Item 2</li>
+    <li>Item 3</li>
+  </ul>
+</section>
+
+<style>
+  section {
+    background-color: red;
+  }
+</style>
